deb6600725aae446d4a1f8725d063e3b0281edd8
David Abulu <dabulu1@sheffield.ac.uk>
2021-03-08 11:22:06 +0000

    Added fields to Conversations migration file

--- db/migrate/20210308081433_create_conversations.rb
+++ db/migrate/20210308081433_create_conversations.rb
@@ -1,7 +1,11 @@
 class CreateConversations < ActiveRecord::Migration[6.0]
   def change
     create_table :conversations do |t|
+      t.bigint :listing_id
+      t.bigint :participant_id
 
+      t.timestamps :created_at
+      t.timestamps :updated_at
       t.timestamps
     end
   end
