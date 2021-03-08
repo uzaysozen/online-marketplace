63f516df8b9c9f2d2edd49d54f35c07010c39c79
David Abulu <dabulu1@sheffield.ac.uk>
2021-03-08 08:38:13 +0000

    Added fields for ListingRatings migration, no foreign keys have been added

--- db/migrate/20210308081734_create_listing_ratings.rb
+++ db/migrate/20210308081734_create_listing_ratings.rb
@@ -1,7 +1,12 @@
 class CreateListingRatings < ActiveRecord::Migration[6.0]
   def change
     create_table :listing_ratings do |t|
+      t.bigint :listing_id
+      t.integer :seller_rating, :null => true
+      t.integer :buyer_rating, :null => true
 
+      t.timestamps :created_at
+      t.timestamps :updated_at
       t.timestamps
     end
   end
