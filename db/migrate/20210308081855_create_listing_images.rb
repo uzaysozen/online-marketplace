95cb1bd971a9be7af2ce28be1cb5a877ad32a1e3
David Abulu <dabulu1@sheffield.ac.uk>
2021-03-08 11:43:47 +0000

    Added fields to ListingImages migration file

--- db/migrate/20210308081855_create_listing_images.rb
+++ db/migrate/20210308081855_create_listing_images.rb
@@ -1,7 +1,10 @@
 class CreateListingImages < ActiveRecord::Migration[6.0]
   def change
     create_table :listing_images do |t|
+      t.string :image_path, :limit => 50
 
+      t.timestamps :created_at
+      t.timestamps :updated_at
       t.timestamps
     end
   end
