5212f2475deb4050f0936a573dddd3e4c67e3b64
David Abulu <dabulu1@sheffield.ac.uk>
2021-03-08 08:52:00 +0000

    Added fields to ListingStatuses migration

--- db/migrate/20210308082029_create_listing_statuses.rb
+++ db/migrate/20210308082029_create_listing_statuses.rb
@@ -1,7 +1,10 @@
 class CreateListingStatuses < ActiveRecord::Migration[6.0]
   def change
     create_table :listing_statuses do |t|
-
+      t.string :name, :limit => 50
+      
+      t.timestamps :created_at
+      t.timestamps :updated_at
       t.timestamps
     end
   end
