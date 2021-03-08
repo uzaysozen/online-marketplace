e734caf8fdb214db6c8d9f77699918f85e7bded6
David Abulu <dabulu1@sheffield.ac.uk>
2021-03-08 08:50:15 +0000

    Added fields to Listings migration, no foreign keys have been added

--- db/migrate/20210308081704_create_listings.rb
+++ db/migrate/20210308081704_create_listings.rb
@@ -1,7 +1,22 @@
 class CreateListings < ActiveRecord::Migration[6.0]
   def change
     create_table :listings do |t|
+      t.boolean :is_active
+      t.boolean :is_moderated
+      t.string :title, :limit => 50
+      t.text :description
+      t.decimal :price
+      t.decimal :discounted_price
+      t.string :location, :limit => 50
+      t.bigint :creator_id
+      t.bigint :moderator_id
+      t.bigint :receiver_id
+      t.bigint :listing_status_id
+      t.bigint :listing_condition_id
+      t.bigint :category_id
 
+      t.timestamps :created_at
+      t.timestamps :updated_at
       t.timestamps
     end
   end
