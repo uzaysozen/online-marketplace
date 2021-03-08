42040bfa8b208bf524abc3107282663025b1a7cd
David Abulu <dabulu1@sheffield.ac.uk>
2021-03-08 11:23:00 +0000

    Added fields to ConversationMessages migration file

--- db/migrate/20210308081533_create_conversation_messages.rb
+++ db/migrate/20210308081533_create_conversation_messages.rb
@@ -1,7 +1,13 @@
 class CreateConversationMessages < ActiveRecord::Migration[6.0]
   def change
     create_table :conversation_messages do |t|
-
+      t.boolean :is_deleted
+      t.text :content
+      t.bigint :conversation_id
+      t.bigint :sender_id
+      
+      t.timestamps :created_at
+      t.timestamps :updated_at
       t.timestamps
     end
   end
