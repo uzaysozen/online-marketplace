42362d0b85cf2c0176f106e0ff3070b6cc22b731
David Abulu <dabulu1@sheffield.ac.uk>
2021-03-08 08:56:06 +0000

    Added fields to Categories migration, no foreign keys have been added

--- db/migrate/20210308082137_create_categories.rb
+++ db/migrate/20210308082137_create_categories.rb
@@ -1,7 +1,10 @@
 class CreateCategories < ActiveRecord::Migration[6.0]
   def change
     create_table :categories do |t|
-
+      t.bigint :parent_id
+      
+      t.timestamps :created_at
+      t.timestamps :updated_at
       t.timestamps
     end
   end
