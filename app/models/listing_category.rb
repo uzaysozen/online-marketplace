02ebbff24dc62793de1b4a5b17c0753412761e22
Uzay Sozen <usozen1@sheffield.ac.uk>
2021-03-09 17:09:54 +0300

    Fix wrong class name in listing category

--- app/models/listing_category.rb
+++ app/models/listing_category.rb
@@ -9,6 +9,6 @@
 #  parent_id  :bigint
 #
 class ListingCategory < ApplicationRecord
-    has_many :children, :class_name => "Category", foreign_key: 'parent_id'
-    belongs_to :parent, :class_name => "Category", foreign_key: 'parent_id', :optional => true
+    has_many :children, :class_name => "ListingCategory", foreign_key: 'parent_id'
+    belongs_to :parent, :class_name => "ListingCategory", foreign_key: 'parent_id', :optional => true
 end
