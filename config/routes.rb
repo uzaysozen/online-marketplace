cc224b81ce20efe81394f6140fa8361f4a0a0c37
David Abulu <dabulu1@sheffield.ac.uk>
2021-03-11 15:02:02 +0000

    Added search route for ConversationMessages

--- config/routes.rb
+++ config/routes.rb
@@ -19,6 +19,9 @@ Rails.application.routes.draw do
   resources :listing_ratings do
     post :search, on: :collection
   end
+  resources :conversation_messages do
+    post :search, on: :collection
+  end
   match "/403", to: "errors#error_403", via: :all
   match "/404", to: "errors#error_404", via: :all
   match "/422", to: "errors#error_422", via: :all
