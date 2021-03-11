b40c7a3ccadee5af6f12b1a3a56966f0746e5c62
David Abulu <dabulu1@sheffield.ac.uk>
2021-03-11 15:41:32 +0000

    Added routes for newly created controllers

--- config/routes.rb
+++ config/routes.rb
@@ -22,6 +22,13 @@ Rails.application.routes.draw do
   resources :conversation_messages do
     post :search, on: :collection
   end
+  resources :conversations
+  resources :listing_images
+  resources :listing_views
+  resources :listings
+  resources :reports
+  resources :user_favourites
+
   match "/403", to: "errors#error_403", via: :all
   match "/404", to: "errors#error_404", via: :all
   match "/422", to: "errors#error_422", via: :all
