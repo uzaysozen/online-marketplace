86f23be495f1b0852ba5551fca6f4a1b62927147
Uzay Sozen <usozen1@sheffield.ac.uk>
2021-03-16 22:06:47 +0300

    Fix bug where message.sender compared with itself

--- app/jobs/send_conversation_message_job.rb
+++ app/jobs/send_conversation_message_job.rb
@@ -1,10 +1,10 @@
 class SendConversationMessageJob < ApplicationJob
   queue_as :default
 
-  def perform(message, user)
+  def perform(message, current_user)
     html = ApplicationController.render(
       partial: 'conversation_messages/conversation_message', 
-      locals: { message: message, user: user }
+      locals: { message: message, current_user: current_user }
     )
     ActionCable.server.broadcast "conversation_channel_#{message.conversation_id}", html: html, message: message
   end
