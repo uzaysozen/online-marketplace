de841f19b463770664d355a7e9ea4c663d14d0cb
Callum Clear <cclear1@sheffield.ac.uk>
2021-03-19 17:50:09 +0000

    fix: pr comments

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,10 +2,10 @@
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 
 # Categories 
-categories = ListingCategory.create([{name: 'Books'}, {name: 'Clothing'}, {name: 'Electronics'}])
+categories = ListingCategory.first_or_create([{name: 'Books'}, {name: 'Clothing'}, {name: 'Electronics'}])
 
 # Item conditions
-conditions = ListingCondition.create([{name: 'New'}, {name: 'Used'}, {name: 'For parts or not working'}])
+conditions = ListingCondition.first_or_create([{name: 'New'}, {name: 'Used'}, {name: 'For parts or not working'}])
 
 # Listing status
-listing_status = ListingStatus.create([{name: 'Pending'}, {name: 'Active'}, {name: 'Complete'}, {name: 'Deleted'}])
+listing_status = ListingStatus.first_or_create([{name: 'Pending'}, {name: 'Active'}, {name: 'Complete'}, {name: 'Deleted'}])
