4b095c1f5fae9c9f787895a34b2e6fb53f9b7ae3
Uzay Sozen <usozen1@sheffield.ac.uk>
2021-04-03 00:55:11 +0300

    fix: revert seed.rb (pushed accidentally)

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,7 +9,3 @@ conditions = ListingCondition.first_or_create([{name: 'New'}, {name: 'Used'}, {n
 
 # Listing status
 listing_status = ListingStatus.first_or_create([{name: 'Pending'}, {name: 'Active'}, {name: 'Complete'}, {name: 'Deleted'}])
-
-listing = Listing.create(is_active: false, is_moderated: false, title: "MSI", description: "A msi computer", price: 200, discounted_price: nil, location: "Crookesmoor", swap: false, listing_category_id: 4, creator_id: 2, moderator_id: 2, receiver_id: 2, listing_condition_id: 1, listing_status_id: 2)
-
-user = User.create(email: "jsmith1@sheffield.ac.uk", username: "aca19js", uid: "aca19js", mail: "jsmith1@sheffield.ac.uk", ou: "COM", dn: "uid=aca19us,ou=Undergraduates,ou=Students,ou=Users...", sn: "Smith", givenname: "John", administrator: nil)
