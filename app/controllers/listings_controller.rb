fbee5c5eceb11f4cb53ed768715b3fcce80a96d1
David Abulu <dabulu1@sheffield.ac.uk>
2021-04-02 20:06:13 +0100

    Added parameter verification

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -5,15 +5,28 @@ class ListingsController < ApplicationController
     def index
       @listings = Listing.all
 
+      # Sorting Function
+      # Empty Parameter Checking
       if params[:sort].nil? or params[:sort] == ""
         params[:sort] = "title"
         sort_order = 'asc'
         @listings = Listing.order("#{params[:sort]} #{sort_order}")
         session[:sort_order] = sort_order == 'asc' ? 'desc' : 'asc'
       else
-        sort_order = session[:sort_order] || 'asc'
-        @listings = Listing.order("#{params[:sort]} #{sort_order}")
-        session[:sort_order] = sort_order == 'asc' ? 'desc' : 'asc'
+        table_col = Listing.column_names
+        sort_val = ['asc', 'desc']
+        
+        # Verifying Parameters
+        if table_col.include? params[:sort] and sort_val.include? session[:sort_order]
+          sort_order = session[:sort_order] || 'asc'
+          @listings = Listing.order("#{params[:sort]} #{sort_order}")
+          session[:sort_order] = sort_order == 'asc' ? 'desc' : 'asc'
+        else
+          params[:sort] = "title"
+          sort_order = 'asc'
+          @listings = Listing.order("#{params[:sort]} #{sort_order}")
+          session[:sort_order] = sort_order == 'asc' ? 'desc' : 'asc'
+        end
       end
     end
 
