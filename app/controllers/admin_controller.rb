4922407c57aaab0249f4a40bd56fc0956af24c31
Uzay Sozen <usozen1@sheffield.ac.uk>
2021-04-16 13:59:14 +0300

    Add admin controller

---
+++ app/controllers/admin_controller.rb
@@ -0,0 +1,15 @@
+class AdminController < ApplicationController
+  
+  # GET admin/moderation
+  def moderation
+  end
+  
+  # GET admin/statistics
+  def statistics
+  end
+  
+  # GET admin/other
+  def other 
+  end
+
+end
