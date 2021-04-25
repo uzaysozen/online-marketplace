9ed61376189cbc95d7d8ce3044a3d7212df8b9a9
aca19fjr <fjrandle1@sheffield.ac.uk>
2021-04-25 23:52:31 +0100

    fix: Update ability file

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -10,7 +10,7 @@ class Ability
       else
         # Listing Restrictions
         can :create, Listing
-        can :read, Listing, listing_status: {name: 'Active'}
+        can :read, Listing, listing_status: { name: 'Active' }
         can [:show, :update, :delete], Listing, creator_id: user.id
 
         # Listing Category
@@ -24,41 +24,19 @@ class Ability
 
         # Conversation
         can :create, Conversation
-        cannot :create, Conversation, listing: {creator_id: user.id}
 
-        can :delete, Conversation, listing: {creator_id: user.id}
+        can :delete, Conversation, listing: { creator_id: user.id }
         can :delete, Conversation, participant_id: user.id
 
         # Favourites
         can :create, UserFavourite
-        cannot :create, UserFavourite, listing: {creator_id: user.id}
       end
+
+      # Declare restrictions that will always apply, such as not
+      # starting a conversation with yourself.
+      cannot :create, Conversation, listing: { creator_id: user.id }
+      cannot :create, UserFavourite, listing: { creator_id: user.id }
+      cannot :create, Report, listing: { creator_id: user.id }
     end
-    # Define abilities for the passed in user here. For example:
-    #
-    #   user ||= User.new # guest user (not logged in)
-    #   if user.admin?
-    #     can :manage, :all
-    #   else
-    #     can :read, :all
-    #   end
-    #
-    # The first argument to `can` is the action you are giving the user
-    # permission to do.
-    # If you pass :manage it will apply to every action. Other common actions
-    # here are :read, :create, :update and :destroy.
-    #
-    # The second argument is the resource the user can perform the action on.
-    # If you pass :all it will apply to every resource. Otherwise pass a Ruby
-    # class of the resource.
-    #
-    # The third argument is an optional hash of conditions to further filter the
-    # objects.
-    # For example, here the user can only update published articles.
-    #
-    #   can :update, Article, :published => true
-    #
-    # See the wiki for details:
-    # https://github.com/CanCanCommunity/cancancan/wiki/Defining-Abilities
   end
 end
