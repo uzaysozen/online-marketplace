cabaf01bf60b9b5057d871f4d9b70f6fda8e08c0
Uzay Sozen <usozen1@sheffield.ac.uk>
2021-04-26 22:11:31 +0300

    Authorize reports

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,5 +1,6 @@
 class ReportsController < ApplicationController
     before_action :set_report, only: [:show, :edit, :update, :destroy]
+    load_and_authorize_resource
   
     # GET /reports
     def index
