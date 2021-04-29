04604b772fee085a6e75342434f8098037b9d1be
aca19fjr <fjrandle1@sheffield.ac.uk>
2021-04-29 11:27:32 +0100

    chore: Document file-upload

--- app/assets/javascripts/file-upload.js
+++ app/assets/javascripts/file-upload.js
@@ -1,3 +1,4 @@
+// This function let's us create a new FileList from an array of files which is not yet directly supported.
 function FileListItems (files) {
     let b = new ClipboardEvent("").clipboardData || new DataTransfer();
     for (let i = 0, len = files.length; i < len; i++) b.items.add(files[i]);
@@ -5,19 +6,22 @@ function FileListItems (files) {
 }
 
 addImagePreview = (file, imageNumber) => {
+    // Ensure the image container exists.
     let container = document.getElementById('images-preview');
     if(container) {
+        // Get the preview container for the next image.
         let preview = document.getElementById('preview_' + imageNumber);
         preview.setAttribute('data-file-name', file.name);
         let image_container = document.getElementById('image_' + imageNumber);
-        
 
+        // Add the image as a background image to the container we got above.
         let image = document.createElement('img');
         image.src = URL.createObjectURL(file);
         image_container.style.backgroundImage = "url(" + image.src + ")";
         image_icon = image_container.children[0];
         image_icon.remove();
 
+        // Create a delete button to remove the image preview.
         let delete_button = document.createElement('span');
         delete_button.classList.add('delete-btn');
         delete_button.setAttribute('del-btn-name', file.name);
@@ -62,6 +66,7 @@ removeImagePreview = (fileName) => {
 
 
 load = (loadEvent) => {
+    // Create array for storing files and ensure our images container exists.
     fileStore = [];
     let el = document.getElementById('listing_images');
     if (el) {
@@ -70,21 +75,27 @@ load = (loadEvent) => {
                 len = fileStore.length
                 // Check for max images
                 if (len < 8) {
+                    // Ensure the user is not adding the same file again.
                     if (!fileStore.map(f => f.name).includes(file.name)) {
+                        // Add the file to the end of the array and add the preview to DOM.
                         fileStore.unshift(file);
                         addImagePreview(file, len + 1);
                     }
                 }
                 else {
+                    // This creates a custom bootstrap notice of our own using the function in notifications.coffee
                     window.notice('Maximum of 8 images per listing');
+                    // Break the loop since we've already reached the limit
                     break;
                 }
             }
         })
     }
 
+    // Ensure the form exists.
     let form = document.getElementById('new_listing');
     if (form) {
+        // On form submit we need to update the files in the pseudo input from our file array.
         form.addEventListener('submit', e => {
             console.log(el.files.length);
             el.files = new FileListItems(fileStore);
@@ -92,6 +103,7 @@ load = (loadEvent) => {
     }
 }
 
+// Declare fileStore array here so it can be easily accessed.
 let fileStore = [];
 
 document.addEventListener('turbolinks:load', load);
