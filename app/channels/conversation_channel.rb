d23ca1d7321950e6b463710aa6a4b1a737340fed
Callum Clear <cclear1@sheffield.ac.uk>
2021-04-30 12:45:04 +0100

    fix: convo lag

--- app/channels/conversation_channel.rb
+++ app/channels/conversation_channel.rb
@@ -1,16 +1,16 @@
 class ConversationChannel < ApplicationCable::Channel
   def subscribed
-    conversation = Conversation.find_by_id(params[:conversation_id])
-    if conversation
-      if can? :read, Conversation
-        stream_from "conversation_channel_#{params[:conversation_id]}"
-      else
-        connection.transmit identifier: params, error: "User is not authorized."
-        reject
-      end
-    else
-      connection.transmit identifier: params, error: "The conversation #{params[:conversation_id]} not found."
-      reject
-    end
+    # conversation = Conversation.find_by_id(params[:conversation_id])
+    # if conversation
+    #   if can? :read, Conversation
+    stream_from "conversation_channel_#{params[:conversation_id]}"
+    #   else
+    #     connection.transmit identifier: params, error: "User is not authorized."
+    #     reject
+    #   end
+    # else
+    #   connection.transmit identifier: params, error: "The conversation #{params[:conversation_id]} not found."
+    #   reject
+    # end
   end
 end
