a1f6de401df602c4dea893403c6f14256b8877c1
Uzay Sozen <usozen1@sheffield.ac.uk>
2021-05-07 00:26:02 +0300

    Add moderating reports and displaying conversation message

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,11 +1,11 @@
 class ReportsController < ApplicationController
-    load_resource only: [:show, :edit, :update, :destroy]
+    load_resource only: [:show, :edit, :update, :destroy, :see_message, :moderate, :moderate_update]
     load_and_authorize_resource
 
     # GET /reports
     def index
-      @message_reports = Report.includes([:listing]).where.not(conversation_message: nil)
-      @listing_reports = Report.includes([:listing]).where.not(listing: nil)
+      @message_reports = Report.includes([:listing, conversation_message: [:sender, :conversation]]).where.not(conversation_message: nil)
+      @listing_reports = Report.includes([:listing, conversation_message: [:sender, :conversation]]).where.not(listing: nil)
     end
   
     # GET /reports/1
@@ -45,6 +45,39 @@ class ReportsController < ApplicationController
     def destroy
       @report.destroy
       redirect_to reports_url, notice: 'Report was successfully deleted.'
+    end
+
+    def see_message
+      render layout: false
+    end
+
+    def moderate
+      render layout: false
+    end
+
+    def moderate_update
+      
+      delete_item = params[:report_options][:delete_item]
+      delete_report = params[:report_options][:delete_report]
+      report_outcome = params[:report_options][:outcome]
+      @report.update(outcome: report_outcome, moderator: current_user)
+      if delete_report == "1"
+        @report.destroy
+      end
+      if delete_item == "1"
+        if @report.listing.present?
+          @report.listing.update(listing_status: ListingStatus.find_by(name: 'Deleted'), moderator: current_user)
+        else
+          @report.conversation_message.update(is_deleted: true)
+        end
+      else
+        if @report.listing.present?
+          @report.listing.update(listing_status: ListingStatus.find_by(name: 'Active'), moderator: current_user)
+        else
+          @report.conversation_message.update(is_deleted: false)
+        end
+      end
+      redirect_to reports_path
     end
   
     private
--- config/routes.rb
+++ config/routes.rb
@@ -44,6 +44,8 @@ Rails.application.routes.draw do
     resources :user_favourites
     resources :conversations do
       resources :conversation_messages, except: :create do
+        get :report, on: :member
+        post :send_report, on: :member
         post :send_message, :as => 'send', on: :member
         patch :delete_message, :as => 'delete', on: :member
       end
@@ -52,7 +54,11 @@ Rails.application.routes.draw do
 
   # Admin Pages
   scope :admin do
-    resources :reports
+    resources :reports do
+      get :see_message, on: :member
+      get :moderate, on: :member
+      post :moderate_update, on: :member
+    end
     get :moderation, to: 'admin#moderation'
     get :statistics, to: 'admin#statistics'
     get :other, to: 'admin#other'
